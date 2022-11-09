382aef486dc227f45cc7d7d9d16fe1319297e5d9
MacBookPro <macbookpro@MacBook-Pro-MacBookPro.local>
2022-11-09 21:35:20 +0300

    Бот хороший день

--- bot.py
+++ bot.py
@@ -1,22 +1,40 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from settings import TOKEN
+from phrases import *
+import random as rn
+from PIL import Image
+img = Image.open(r'images_mem.jpeg')
+
 
 def sms(bot,update):
     print('Кто-то отправил /start Что делать?')
-    bot.message.reply_text(f'Здравствуйте, {bot.message.chat.first_name}, я бот!\nПока я еще ничего не умею, но я быстро учусь!🥺')
+    bot.message.reply_text(f'Здравствуйте, {bot.message.chat.first_name}, я бот!\n\
+    Из возможных функций я могу вам предложить: пока только пособолезнуйте🥺')
     print(bot.message)
 
-def parrot(bot, update):
-    print(bot.message.text)
-    bot.message.reply_text(bot.message.text)
+def memas(bot, update):
+    bot.message.reply_photo('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPZuDbLEFATlFvmdhcTO2RDtSzWg-B1v59LB5coUSCBW58BXaTYsximkAAMQ2AUaB1RvI&usqp=CAU')
+
+def otvetka(bot, update):
+    mes = bot.message.text.lower()
+    print(mes)
+    if mes in hello_user:
+        bot.message.reply_text(rn.choice(hello_bot)+", "+rn.choice(appeal_bot))
+    elif mes in appeal_user:
+        bot.message.reply_text('ЧЁ?')
+    elif mes in bye_user:
+        bot.message.reply_text(rn.choice(bye_bot))
+    else:
+        bot.message.reply_text('Я вась не панимат!')
 
 
 def main():
     my_bot = Updater(TOKEN)
 
     my_bot.dispatcher.add_handler(CommandHandler('start', sms))
+    my_bot.dispatcher.add_handler(CommandHandler('mem', memas))
 
-    my_bot.dispatcher.add_handler(MessageHandler(Filters.text, parrot))
+    my_bot.dispatcher.add_handler(MessageHandler(Filters.text, otvetka))
 
     my_bot.start_polling()  # проверяет наличие сообщений
     my_bot.idle()  # будет работать, пока его не остановят
